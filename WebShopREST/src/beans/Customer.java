05c8f0c762fda17bfe4728d513eaeb0d9c31025f
Gravarica <93195018+Gravarica@users.noreply.github.com>
2022-06-17 01:26:49 +0200

    Added customer class

--- WebShopREST/src/beans/Customer.java
+++ WebShopREST/src/beans/Customer.java
@@ -1,7 +1,31 @@
 package beans;
 
+import enums.Gender;
+import enums.UserType;
+
 public class Customer extends User{
 
+	private double membershipFee;
+	//private ArrayList<SportObject> objectsVisited;
+
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Customer(String username, String password, String firstName, String lastName, Gender gender,
+			UserType userType) {
+		super(username, password, firstName, lastName, gender, userType);
+		// TODO Auto-generated constructor stub
+	}
+
+	public double getMembershipFee() {
+		return membershipFee;
+	}
+
+	public void setMembershipFee(double membershipFee) {
+		this.membershipFee = membershipFee;
+	}
 	
 	
 }
