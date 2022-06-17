552c1d8268dc693ec8c02af2b51f8c715115db9a
Andrija25 <93194317+Andrija25@users.noreply.github.com>
2022-06-18 00:41:30 +0200

    Added json files

--- WebShopREST/src/services/SportObjectService.java
+++ WebShopREST/src/services/SportObjectService.java
@@ -6,6 +6,7 @@ import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -38,6 +39,13 @@ public class SportObjectService {
 	@Produces(MediaType.APPLICATION_JSON)
 	public ArrayList<SportObject> getAll(){
 		return getSportObjectDAO().getAll();
+	}
+	
+	@GET
+	@Path("/get/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public SportObject getById(@PathParam("id") int id) {
+		return getSportObjectDAO().getById(id);
 	}
 	
 }
