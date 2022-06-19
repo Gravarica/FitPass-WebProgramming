40094279d229c8ec6ccee6c90cf5ceed9faceede
Gravarica <93195018+Gravarica@users.noreply.github.com>
2022-06-19 02:27:58 +0200

    Added one function to js

--- WebShopREST/WebContent/js/app.js
+++ WebShopREST/WebContent/js/app.js
@@ -5,13 +5,15 @@ const ArtikliForma = { template: '<unos-artikla></unos-artikla>'}
 const PrikazArtikala = { template: '<prikaz-artikala></prikaz-artikala>'}
 const Product = { template: '<edit-product></edit-product>' }
 const Products = { template: '<products></products>' }
+const Login = {template: '<login></login>' }
 const SearchProduct = {template: '<search></search>' }
 
 const router = new VueRouter({
 	mode: 'hash',
 	  routes: [
 		{ path: '/', name: 'home', component: Products},
-	    { path: '/product', component: Product}
+	    { path: '/product', component: Product},
+	    { path: '/login', component: Login }
 	  ]
 });
 
@@ -19,5 +21,9 @@ var app = new Vue({
 	router,
 	el: '#main',
 	data: {
+		userStatus = 'NEREGISTROVAN'
+	},
+	methods: {
+		
 	}
 });
