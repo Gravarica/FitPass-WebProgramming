8975b2426fa766a8152447215cea23bda683620b
Andrija25 <93194317+Andrija25@users.noreply.github.com>
2022-06-29 05:06:05 +0200

    nav bar added

--- front-vue3/src/views/HomeView.vue
+++ front-vue3/src/views/HomeView.vue
@@ -1,8 +1,4 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
 </template>
 
 <script>
@@ -16,3 +12,9 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+
+
+</style>
